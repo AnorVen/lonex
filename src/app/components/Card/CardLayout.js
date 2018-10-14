641173849af1fa944cc3fbc947f168308a438ef3
AnorVen <efremovgs@gmail.com>
2018-10-14 13:50:05 +0300

    product done

--- src/app/components/Card/CardLayout.js
+++ src/app/components/Card/CardLayout.js
@@ -1,68 +1,68 @@
-import React, { Component } from "react";
-import Addtoorder from "./addToOrder";
-import Product from "./products";
-import tov1 from "../../../imgs/tov1.jpg";
-import tov2 from "../../../imgs/tov2.jpg";
-import tov3 from "../../../imgs/tov3.jpg";
-import tov4 from "../../../imgs/tov3.jpg";
+import React, { Component } from 'react';
+import Addtoorder from './addToOrder';
+import Product from './products';
+import tov1 from '../../../imgs/tov1.jpg';
+import tov2 from '../../../imgs/tov2.jpg';
+import tov3 from '../../../imgs/tov3.jpg';
+import tov4 from '../../../imgs/tov3.jpg';
 
 const products = [
-  {
-    title: "Платье-миди с расклешенной юбкой",
-    img: tov1,
-    price: 2450,
-    code: "82039-11",
-    size: "44",
-    color: "синий",
-    quantity: 1
-  },
-  {
-    title: "Туфли женские украшенные кружевными вставками",
-    img: tov1,
-    price: 2450,
-    code: "13524-01",
-    size: 38,
-    color: "черный",
-    quantity: 1
-  },
-  {
-    title: "Платье-миди",
-    img: tov1,
-    price: 2450,
-    code: "75039",
-    size: 44,
-    color: "белый",
-    quantity: 1
-  },
-  {
-    title: "Платье-миди",
-    img: tov1,
-    price: 2450,
-    code: "75039",
-    size: 46,
-    color: "белый",
-    quantity: 1
-  }
+	{
+		title: 'Платье-миди с расклешенной юбкой',
+		img: tov1,
+		price: 2450,
+		code: '82039-11',
+		size: '44',
+		color: 'синий',
+		quantity: 1,
+	},
+	{
+		title: 'Туфли женские украшенные кружевными вставками',
+		img: tov2,
+		price: 2450,
+		code: '13524-01',
+		size: 38,
+		color: 'черный',
+		quantity: 1,
+	},
+	{
+		title: 'Платье-миди',
+		img: tov3,
+		price: 2450,
+		code: '75039',
+		size: 44,
+		color: 'белый',
+		quantity: 1,
+	},
+	{
+		title: 'Платье-миди',
+		img: tov3,
+		price: 2450,
+		code: '75039',
+		size: 46,
+		color: 'белый',
+		quantity: 1,
+	},
 ];
 
 export default class CardLayout extends Component {
-  render() {
-    return (
-      <div className="card">
-        <div className="wrapper">
-          <div className="card__title">
-            <h3>Ваша корзина</h3>
-          </div>
-          {products ? (
-            <Product />
-          ) : (
-            <div className="card__poproducts">
-              <h3>Карзина пуста!</h3>
-            </div>
-          )}
-          <Addtoorder />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="card">
+				<div className="wrapper">
+					<div className="card__title">
+						<h3>Ваша корзина</h3>
+					</div>
+					{products ? (
+						<Product products={products} />
+					) : (
+						<div className="card__poproducts">
+							<h3>Карзина пуста!</h3>
+						</div>
+					)}
+					<Addtoorder />
+				</div>
+			</div>
+		);
+	}
 }
--- src/app/components/Card/products.js
+++ src/app/components/Card/products.js
@@ -1,24 +1,70 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react';
+import cansel from '../../../imgs/cancel.png';
 
 export default class Product extends Component {
-  render() {
-    return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Товар</th>
-              <th>Описание</th>
-              <th>Количество</th>
-              <th>Цена</th>
-              <th>Удалить</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr />
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: [],
+		};
+	}
+	componentDidMount() {
+		this.setState({
+			products: this.props.products,
+		});
+	}
+
+	render() {
+		console.log(this.state);
+		let products = this.state.products.map(function(item, index) {
+			return (
+				<div className="product__item" key={index}>
+					<div className="product__img">
+						<img src={item.img} alt={item.title} />
+					</div>
+					<div className="product__info">
+						<h4 className="product__title">{item.title}</h4>
+						<span className="product__code">Код: {item.code}</span>
+						<span className="product__size">Размер: {item.size}</span>
+						<span className="product__color">Цвет: {item.colon}</span>
+					</div>
+					<div className="product__quantity">
+						<button className="quantity__dic">-</button>
+						<span className="quantity__res">{item.quantity}</span>
+						<button className="quantity__inc">+</button>
+					</div>
+					<div className="product__price">
+						<span className="price">{item.price} руб.</span>
+					</div>
+					<div className="product__del">
+						<button className="del__btn">
+							<img src={cansel} alt="удалить товар" />
+						</button>
+					</div>
+				</div>
+			);
+		});
+		return (
+			<div className="product">
+				<div className="product__header">
+					<div className="product__title">
+						<span>Товар</span>
+					</div>
+					<div className="product__title">
+						<span>Описание</span>
+					</div>
+					<div className="product__title">
+						<span>Количество</span>
+					</div>
+					<div className="product__title">
+						<span>Цена</span>
+					</div>
+					<div className="product__title">
+						<span>Удалить</span>
+					</div>
+				</div>
+				<div className="product__list">{products}</div>
+			</div>
+		);
+	}
 }
